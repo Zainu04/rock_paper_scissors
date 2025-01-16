33ae73dd3c2acbc81280031c78807353bf87161e
Zainu04 <akhtarzainab2010@gmail.com>
2025-01-16 14:55:30 -0500

    fix issue with image path

--- uijavascript.js
+++ uijavascript.js
@@ -1,7 +1,7 @@
 const game = () => {
     let pScore = 0;
     let cScore = 0;
-
+=
     const startGame = () => {
         const playBtn = document.querySelector(".intro button");
         const introScreen = document.querySelector(".intro");
